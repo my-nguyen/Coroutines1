da54f797e7f7c73c9fd53a8508772257b85be21f
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-15 15:33:14 -0700

    b2: manually downloaded bitmap then displayed it

--- app/src/main/java/com/nguyen/coroutines1/MainActivity.kt
+++ app/src/main/java/com/nguyen/coroutines1/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.nguyen.coroutines1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import com.bumptech.glide.Glide
+import android.os.StrictMode
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.nguyen.coroutines1.databinding.ActivityMainBinding
+import java.net.URL
 
 private const val IMAGE_URL = "https://rkpandey.com/images/rkpDavidson.jpg"
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +18,24 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Glide.with(this).load(IMAGE_URL).into(binding.image)
+        // bypass NetworkOnMainThread exception; not recommended for production
+        val policy = StrictMode.ThreadPolicy.Builder().permitNetwork().build()
+        StrictMode.setThreadPolicy(policy)
+        val bitmap = downloadBitmap(IMAGE_URL)
+        binding.image.setImageBitmap(bitmap)
+    }
+
+    private fun downloadBitmap(url: String): Bitmap? {
+        return try {
+            val connection = URL(url).openConnection()
+            connection.connect()
+            val inputStream = connection.getInputStream()
+            val bitmap = BitmapFactory.decodeStream(inputStream)
+            inputStream.close()
+            bitmap
+        } catch (e: Exception) {
+            Log.e(TAG, "Exception $e")
+            null
+        }
     }
 }
