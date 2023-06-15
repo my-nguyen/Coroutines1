fdd832881b14111a8b706c32a6af3d1e182067de
Mỹ Nguyễn <nguyen@Mys-Laptop.local>
2023-06-15 15:37:21 -0700

    b4: used coroutines to switch context between IO and Main thread

--- app/src/main/java/com/nguyen/coroutines1/MainActivity.kt
+++ app/src/main/java/com/nguyen/coroutines1/MainActivity.kt
@@ -3,13 +3,14 @@ package com.nguyen.coroutines1
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.nguyen.coroutines1.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.net.URL
-import kotlin.concurrent.thread
 
 private const val IMAGE_URL = "https://rkpandey.com/images/rkpDavidson.jpg"
 private const val TAG = "MainActivity"
@@ -20,13 +21,11 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // create a thread to do the image download, then send bitmap to the main thread
-        val handler = Handler(Looper.getMainLooper())
-        thread(start=true) {
+        CoroutineScope(Dispatchers.IO).launch {
             Log.i(TAG, "Current thread ${Thread.currentThread().name}")
             val bitmap = downloadBitmap(IMAGE_URL)
-            handler.post {
-                Log.i(TAG, "Current thread in Handler: ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                Log.i(TAG, "Current thread in main dispatcher: ${Thread.currentThread().name}")
                 binding.image.setImageBitmap(bitmap)
             }
         }
